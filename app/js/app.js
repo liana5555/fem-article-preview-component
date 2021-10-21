b8378720264be76e8f2ee4aa04b8d48d63220120
Ildikó Balogh <baloghildiko55@gmail.com>
2021-10-21 21:07:13 +0200

    mobile version with functionality

---
+++ app/js/app.js
@@ -0,0 +1,54 @@
+// targeting the button and the info and social container
+
+const infoContainer = document.querySelector('.info-container');
+const socialContainer = document.querySelector('.social-container');
+const shares = document.querySelectorAll('.icon');
+
+shares.forEach(function(share) {
+
+
+share.addEventListener('click' , function() {
+    if (infoContainer.classList.contains('hidden')) {
+        console.log('true info contains hidden');
+
+        infoContainer.classList.remove('fade-out');
+
+        socialContainer.classList.add('fade-out');
+        socialContainer.classList.remove('fade-in');
+
+
+        socialContainer.classList.add('hidden');
+
+        infoContainer.classList.remove('hidden');       
+        infoContainer.classList.add('fade-in');
+
+       
+      
+    }
+    else {
+        console.log('false');
+
+        socialContainer.classList.remove('fade-out');
+
+        infoContainer.classList.add('fade-out');
+
+
+        infoContainer.classList.add('hidden');
+        
+        
+        
+        socialContainer.classList.remove('hidden');
+
+        socialContainer.classList.add('fade-in');
+
+        
+        infoContainer.classList.remove('fade-in');
+            
+        
+      
+    }
+
+}) ;
+
+
+});
